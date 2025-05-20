6b83030b124349fbf98db82fc7a5468e5bb5b341
NatalieGichuki <nataliegichuki@gmail.com>
2025-05-20 20:36:34 +0300

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Mr. Chocolate', 3, 'M', 'Brown', 'angry', TRUE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Rowdy', 8, 'F', 'Black', 'calm', TRUE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Tabitha', 2, 'F', 'Brown', 'aggitated', TRUE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Sergeant Brown', 1, 'M', 'White', 'calm', FALSE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Melissa', 6, 'F', 'Black', 'calm', TRUE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Grinch', 4, 'M', 'White', 'aggitated', FALSE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Wendy', 7, 'F', 'Brown', 'aggitated', TRUE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(NULL, 10, 'M', 'Grey', 'calm', TRUE);
